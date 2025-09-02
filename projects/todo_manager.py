5cfb59a2de58749df1a0370d632dd96e44bd7992
Awani Caleb <calebawani@gmail.com>
2025-09-02 07:15:35 +0100

    feat: Add functionality to create new todos

--- projects/todo_manager.py
+++ projects/todo_manager.py
@@ -1,12 +1,28 @@
 import requests
-
+import random
 
 class TodoManager():
     def __init__(self) -> None:
         self.request_session = requests.Session()
         pass
 
-    def get_all_todos(self) -> dict:
+    def create_todo(self, todo: dict) -> dict:
+        try:
+            response = self.request_session.post(
+                "https://jsonplaceholder.typicode.com/todos",
+                json=todo
+            )
+            response.raise_for_status()
+
+            print("To-do item created successfully!")
+            return response.json()
+
+        except requests.exceptions.HTTPError as e:
+            print(f"Oops! Something went wrong: {e}")
+            return None
+
+
+    def get_all_todos(self):
         all_todos = self.request_session.get("https://jsonplaceholder.typicode.com/todos")
 
         try:
@@ -21,4 +37,16 @@ class TodoManager():
 if __name__ == "__main__":
     mgt = TodoManager()
 
-    print(mgt.get_all_todos())
+    new_todo = {
+        'title': 'Learn about APIs',
+        'completed': False,
+        'userId': 1
+    }
+
+    created_todo = mgt.create_todo(new_todo)
+
+    if created_todo:
+        print("\n--- New To-do Item ---")
+        print(created_todo)
+
+        # feat: Add functionality to create new todos
